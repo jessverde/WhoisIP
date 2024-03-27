2ad3dcd7ea155baf370847484f7e2bcb3ef617a6
jessverde <164936767+jessverde@users.noreply.github.com>
2024-03-27 18:53:42 -0400

    Add files via upload

---
+++ import requests.py
@@ -0,0 +1,31 @@
+import requests
+
+def get_ip_info(ip):
+    try:
+        response = requests.get(f'http://ip-api.com/json/{ip}')
+        response.raise_for_status()
+        data = response.json()
+        return {
+            'country': data.get('country'),
+            'city': data.get('city'),
+            'countryCode': data.get('countryCode'),
+            'continent': data.get('continent')
+        }
+    except requests.RequestException as e:
+        print(f'Error: {e}')
+        return None
+
+# List of IP addresses to look up
+ip_addresses = ['24.99.24.212']
+
+# Loop through the list of IP addresses and print out information
+for ip in ip_addresses:
+    info = get_ip_info(ip)
+    if info:
+        print(f"Details for IP: {ip}")
+        print(f"Country: {info['country']}")
+        print(f"City: {info['city']}")
+        print(f"Country Code: {info['countryCode']}")
+        print(f"Continent: {info['continent']}")
+        print('---------------------------------')
+
